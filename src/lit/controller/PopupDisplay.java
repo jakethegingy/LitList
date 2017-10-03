797ff81d1b3c77b70d715732e4d0312a477f4fdd
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-10-03 09:14:50 -0600

    Added whole new class

---
+++ src/lit/controller/PopupDisplay.java
@@ -0,0 +1,20 @@
+package lit.controller;
+
+import javax.swing.JOptionPane;
+
+public class PopupDisplay 
+{
+	public void displayText(String message)
+	{
+		JOptionPane.showMessageDialog(null, message);
+	}
+	
+	public String getResponse(String fromQuestion)
+	{
+		String response = "";
+		
+		response += JOptionPane.showInputDialog(null, fromQuestion);
+		
+		return response;
+	}
+}
